ba1b3a23974cf60c02d094a0c303ecfb6d21b53a
Hana Baskin <hana.baskin@gmail.com>
2026-08-12 11:25:54 -0400

    Update hot dog lengths

--- src/data/hotdogs.ts
+++ src/data/hotdogs.ts
@@ -17,13 +17,33 @@ export const hotdogs: Brand[] = [
     products: [
       {
         id: 101,
-        name: "Skinless Beef Franks",
-        length_inches: 6.5,
+        name: "Skinless Bun Length",
+        length_inches: 5.5,
       },
       {
         id: 102,
-        name: "Bun Length Franks",
-        length_inches: 8.0,
+        name: "Colossal",
+        length_inches: 7.0,
+      },
+      {
+        id: 103,
+        name: "Coney Island",
+        length_inches: 7.5,
+      },
+      {
+        id: 104,
+        name: "Uncured Beef",
+        length_inches: 5.5,
+      },
+      {
+        id: 105,
+        name: "Angus Beef",
+        length_inches: 6.0,
+      },
+      {
+        id: 106,
+        name: "Jumbo",
+        length_inches: 6.0,
       },
     ],
   },
@@ -34,7 +54,22 @@ export const hotdogs: Brand[] = [
       {
         id: 201,
         name: "Beef Franks",
-        length_inches: 6.0,
+        length_inches: 5.5,
+      },
+      {
+        id: 202,
+        name: "Bun Length Beef Franks",
+        length_inches: 6.25,
+      },
+      {
+        id: 203,
+        name: "Jumbo",
+        length_inches: 5.5,
+      },
+      {
+        id: 204,
+        name: "All Natural Uncured",
+        length_inches: 5.5,
       },
     ],
   },
@@ -45,18 +80,103 @@ export const hotdogs: Brand[] = [
       {
         id: 301,
         name: "Classic Beef Franks",
-        length_inches: 5.5,
+        length_inches: 5.6,
+      },
+      {
+        id: 302,
+        name: "Bun Length",
+        length_inches: 6.0,
       },
     ],
   },
   {
     id: 4,
-    name: "Costco",
+    name: "Ball Park Brand",
     products: [
       {
         id: 401,
-        name: "Foot Long",
-        length_inches: 12,
+        name: "Beef",
+        length_inches: 5.0,
+      },
+      {
+        id: 402,
+        name: "Bun Size Beef",
+        length_inches: 5.5,
+      },
+      {
+        id: 403,
+        name: "Angus",
+        length_inches: 5.0,
+      },
+      {
+        id: 404,
+        name: "Angus Bun Size",
+        length_inches: 6.0,
+      },
+      {
+        id: 405,
+        name: "Franks",
+        length_inches: 5.0,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Sahlen's",
+    products: [
+      {
+        id: 501,
+        name: "Smokehouse Pork/Beef",
+        length_inches: 7.0,
+      }
+    ],
+  },
+  {
+    id: 6,
+    name: "Applegate",
+    products: [
+      {
+        id: 601,
+        name: "Natural Uncured Beef",
+        length_inches: 5.0,
+      },
+      {
+        id: 602,
+        name: "The Great Organic Beef",
+        length_inches: 5.0,
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Carolina",
+    products: [
+      {
+        id: 701,
+        name: "Bright Leaf",
+        length_inches: 5.0,
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Costco",
+    products: [
+      {
+        id: 801,
+        name: "Food Court",
+        length_inches: 8,
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Sam's Club",
+    products: [
+      {
+        id: 901,
+        name: "Food Court",
+        length_inches: 7,
       },
     ],
   },
